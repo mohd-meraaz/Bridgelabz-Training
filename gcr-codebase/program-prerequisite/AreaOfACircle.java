c6a2302b7e7ab451cdb19f3ffac92a4a3db9a009
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2025-12-16 17:26:27 +0530

    [MohdMeraaz] : added Area of a Circle program

---
+++ gcr-codebase/program-prerequisite/AreaOfACircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaOfACircle{
+	public static void main(String[] args){
+		Scanner Sc = new Scanner(System.in);
+		double radius = Sc.nextDouble();
+		double area = 3.14 * radius * radius;
+		System.out.println("The area is " + area);
+
+	}
+}
