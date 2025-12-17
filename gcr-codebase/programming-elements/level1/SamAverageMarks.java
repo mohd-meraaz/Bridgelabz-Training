d396814c655390773f40283453d04822ec622389
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2025-12-17 12:52:35 +0530

    [MohdMeraaz] : Added code of calculating average marks of Sam

---
+++ gcr-codebase/programming-elements/level1/SamAverageMarks.java
@@ -0,0 +1,17 @@
+// Writing java program for calculating average marks
+
+
+public class SamAverageMarks{
+	public static void main(String[] args){
+	// Create int variables to assign value maths, physics, chemistry marks;
+	int mathsMarks = 94, physicsMarks = 95, chemistryMarks = 96;
+
+	// Create a int variable averageMarks and calculating the value of Sam’s average mark
+	double averageMarks = (double)(mathsMarks+physicsMarks+chemistryMarks)/3;
+
+	// Display the result
+	System.out.println (" Sam’s average mark in PCM is " + averageMarks);
+}
+
+
+}
