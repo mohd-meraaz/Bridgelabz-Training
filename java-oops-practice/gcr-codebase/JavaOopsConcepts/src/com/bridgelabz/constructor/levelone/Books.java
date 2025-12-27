7ac25ab2cf1b6d5d576057d7a9e9d17362a5ebda
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2025-12-27 16:13:30 +0530

    [MohdMeraaz] : added Book Constructor class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/Books.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.constructor.levelone;
+
+public class Books {
+	
+	String title;
+	String author;
+	int price;
+	Books(String title, String author, int price){
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+	
+	Books(){
+		System.out.println("Object Created");
+	}
+	
+	public void display() {
+		System.out.println("Title : " + this.title);
+		System.out.println("Author : " + this.author);
+		System.out.println("Price : " + this.price);
+		
+	}
+
+	public static void main(String[] args) {
+		Books Monk = new Books("The Monk Who Sold His Ferrari","Robin Sharma",225);
+		Monk.display();
+		
+		
+
+	}
+
+}
