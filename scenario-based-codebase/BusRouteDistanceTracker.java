e4b888198270ae18cd25e5c5c38d94721c46e442
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-01 16:24:07 +0530

    [MohdMeraaz] : added java code of Bus Route Distance Tracker

---
+++ scenario-based-codebase/BusRouteDistanceTracker.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BusRouteDistanceTracker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalDistance = 0;
+
+        while (true) {
+            System.out.print("Enter distance to next stop (km): ");
+            int distance = sc.nextInt();
+            totalDistance += distance;
+
+            System.out.print("Do you want to get off at this stop? (yes/no): ");
+            String choice = sc.next();
+
+            if (choice.equalsIgnoreCase("yes")) {
+                break;
+            }
+        }
+
+        System.out.println("Total distance travelled: " + totalDistance + " km");
+        sc.close();
+    }
+}
