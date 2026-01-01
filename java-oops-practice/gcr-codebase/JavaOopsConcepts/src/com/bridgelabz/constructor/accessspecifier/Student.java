49a13a5b56c7131cfb305b19b6464397f545e64e
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-01 11:45:05 +0530

    [MohdMeraaz] : modified Student subclasses and main class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/accessspecifier/Student.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/accessspecifier/Student.java
@@ -26,25 +26,4 @@ class Student {
 }
 
 // Subclass
-class PostgraduateStudent extends Student {
 
-    PostgraduateStudent(int rollNumber, String name, double cgpa) {
-        super(rollNumber, name, cgpa);
-    }
-
-    void displayDetails() {
-        System.out.println("Roll Number: " + rollNumber); // public
-        System.out.println("Name: " + name);               // protected
-        System.out.println("CGPA: " + getCGPA());           // private via method
-    }
-
-    
-}
-
-class Main{
-	public static void main(String[] args) {
-        PostgraduateStudent pg = new PostgraduateStudent(101, "Rahul", 8.5);
-        pg.displayDetails();
-    }
-	
-}
