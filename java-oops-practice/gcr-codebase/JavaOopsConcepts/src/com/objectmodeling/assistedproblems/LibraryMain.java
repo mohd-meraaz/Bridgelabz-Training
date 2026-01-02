ce6e449de4cad79c35cd59a9ca1fc6a70bfcca79
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-02 11:55:34 +0530

    [MohdMeraaz] : added main class and subclasses of Library

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/assistedproblems/LibraryMain.java
@@ -0,0 +1,20 @@
+package com.objectmodeling.assistedproblems;
+
+public class LibraryMain {
+	public static void main(String[] args) {
+		// Books created independently
+		Book b1 = new Book("Godan", "Premchand", 2390);
+        Book b2 = new Book("Gunah", "Dharm", 1110);
+
+        // Library object
+        Library library = new Library();
+
+        // Adding books to library (Aggregation)
+        library.addBook(b1);
+        library.addBook(b2);
+
+        // Display books
+        library.displayLibraryBooks();
+	}
+
+}
