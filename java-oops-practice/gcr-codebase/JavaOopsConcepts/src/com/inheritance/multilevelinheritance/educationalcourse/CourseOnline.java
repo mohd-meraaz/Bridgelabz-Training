4369c2b9890a974fd2f600c7cd7fddf21613a75a
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-04 01:18:00 +0530

    [MohdMeraaz] : Added Educational Course Hierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/educationalcourse/CourseOnline.java
@@ -0,0 +1,8 @@
+package com.inheritance.multilevelinheritance.educationalcourse;
+
+// onlinecourse extends course
+public class CourseOnline extends Course {
+
+    String platform;
+    boolean isRecorded;
+}
