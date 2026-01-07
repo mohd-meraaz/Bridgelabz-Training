79c53cee946d884465933693e52e8abd3ae64eb3
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-08 01:18:05 +0530

    [MohdMeraaz] : Added ReverseString.java

---
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/string/ReverseString.java
@@ -0,0 +1,37 @@
+package com.string;
+
+import java.util.*;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read input
+        System.out.print("Enter a String : ");
+        String input = sc.nextLine().trim();
+
+        // Convert to StringBuilder
+        StringBuilder inputString = new StringBuilder(input);
+
+        // Reverse the string
+
+        StringBuilder outputString = reverseString(inputString);
+
+        // Output result
+        System.out.println(outputString);
+
+        sc.close();
+    }
+
+    private static StringBuilder reverseString(StringBuilder inputString) {
+
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversedString.append(inputString.charAt(i));
+        }
+
+        return reversedString;
+    }
+
+}
