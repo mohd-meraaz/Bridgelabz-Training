599560ed0587a5d82811656b1d2b638630e8d78a
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-08 11:55:57 +0530

    [MohdMeraaz] : added CompareStringBufferAndStringBuilder.java

---
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/stringandsorting/stringbuilder/CompareStringBufferAndStringBuilder.java
@@ -0,0 +1,35 @@
+package com.stringandsorting.stringbuilder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class CompareStringBufferAndStringBuilder {
+	public static void main(String[] args) throws Exception{
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter a string : ");
+		String s = read.readLine();
+		StringBuilder str = new StringBuilder();
+		
+		long t1 = System.nanoTime();
+		for(char ch : s.toCharArray()) {
+			str.append(ch);
+		}
+		long t2 = System.nanoTime();
+		long stringbuilder = t2-t1;
+		
+		StringBuffer strbuffer = new StringBuffer();
+		t1 = System.nanoTime();
+		for(char ch : s.toCharArray()) {
+			strbuffer.append(ch);
+		}
+		 t2 = System.nanoTime();
+		 long stringbuffer = t2-t1;
+		  
+		 if(stringbuffer>stringbuilder) {
+			 System.out.println("String buffer takes more time");
+		 }else {
+			 System.out.println("String Builder take more time");
+		 }
+	}
+
+}
