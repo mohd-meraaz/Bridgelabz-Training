37255e133ffc63f8be69e5bdb1f52ff0ad4a84d7
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-08 11:43:10 +0530

    [MohdMeraaz] : added RemoveDuplicate.java

---
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/string/stringbuilder/RemoveDuplicate.java
@@ -0,0 +1,20 @@
+package com.string.stringbuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RemoveDuplicate {
+	public static void main(String[] args) throws Exception {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		String s = read.readLine();
+		StringBuilder str = new StringBuilder();
+		
+		for(char ch: s.toCharArray()) {
+			if(!str.toString().contains(""+ch)){
+				str.append(ch);
+			}
+		}
+		System.out.println("String After Removing Duplicates "+str.toString());
+	}
+
+}
