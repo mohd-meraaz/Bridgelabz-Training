5e479d99c13e89e2fbe3933c892d4d8157fc323b
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-08 13:28:54 +0530

    [MohdMeraaz] : updated Search2DMatrix.java

--- dsa-practice/gcr-codebase/DSAPractice/src/com/stringandsorting/searching/Search2DMatrix.java
+++ dsa-practice/gcr-codebase/DSAPractice/src/com/stringandsorting/searching/Search2DMatrix.java
@@ -17,35 +17,27 @@ public class Search2DMatrix {
         System.out.println("Target found: " + found);
     }
 
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
 
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
+        int low = 0, high = n * m - 1;
+        
+        // Perform binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            // Calculate the row and column
+            int row = mid / m;
+            int col = mid % m;
+            
+            // If target is found return true
+            if (mat[row][col] == target) return true;
+            else if (mat[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
-
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        int left = 0;
-        int right = rows * cols - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            int row = mid / cols;
-            int col = mid % cols;
-
-            int midValue = matrix[row][col];
-
-            if (midValue == target) {
-                return true;
-            } else if (midValue > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return false;
+        
+        // Return false if target is not found
+        return false; 
     }
 }
