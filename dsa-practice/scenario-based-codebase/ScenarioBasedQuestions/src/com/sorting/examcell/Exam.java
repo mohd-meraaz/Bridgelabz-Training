9aa3cfa9fcdef87e2942b3aa6dfbb7407ba41685
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-18 00:50:02 +0530

    [MohdMeraaz] : added examcell code

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedQuestions/src/com/sorting/examcell/Exam.java
@@ -0,0 +1,14 @@
+package com.sorting.examcell;
+
+public class Exam {
+
+    String studentName;
+    int studentRank;
+    int studentScore;
+
+    public Exam(String studentName, int studentRank, int studentScore) {
+        this.studentName = studentName;
+        this.studentRank = studentRank;
+        this.studentScore = studentScore;
+    }
+}
