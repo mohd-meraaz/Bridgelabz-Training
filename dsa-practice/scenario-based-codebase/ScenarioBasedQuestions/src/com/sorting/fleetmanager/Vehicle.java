5636dc350e731de0168c0e6c0ad92491bdaaf2bf
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-18 23:21:44 +0530

    [MohdMeraaz] : added FleetManager – Vehicle Maintenance Scheduler.

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedQuestions/src/com/sorting/fleetmanager/Vehicle.java
@@ -0,0 +1,11 @@
+package com.sorting.fleetmanager;
+
+public class Vehicle {
+	String vehicleName;
+	int mileage;
+	
+	public Vehicle(String vehicleName, int mileage) {
+		this.vehicleName = vehicleName;
+		this.mileage = mileage;
+	}
+}
