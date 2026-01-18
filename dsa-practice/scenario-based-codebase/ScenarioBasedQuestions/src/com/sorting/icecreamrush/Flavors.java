c8e996a7cc7fc709e0f0364877330b422a082a63
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-18 23:25:22 +0530

    [MohdMeraaz] : added icecreamrush

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedQuestions/src/com/sorting/icecreamrush/Flavors.java
@@ -0,0 +1,11 @@
+package com.sorting.icecreamrush;
+
+public class Flavors {
+	String name;
+	int sold;
+	
+	public Flavors(String name, int sold) {
+		this.name = name;
+		this.sold = sold;
+	}
+}
---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedQuestions/src/com/sorting/icecreamrush/IceCreamRushApp.java
@@ -0,0 +1,27 @@
+package com.sorting.icecreamrush;
+
+public class IceCreamRushApp {
+	public static void main(String[] args) {
+		Flavors flavors[] = {new Flavors("Vanilla",20), new Flavors("Chocolate",120), new Flavors("Strawberry",10), new Flavors("Kulfi",15), new Flavors("Green Tea",5), new Flavors("Red Velvet",43), new Flavors("Mango",100), new Flavors("Black Chocolate",220)};
+		bubbleSort(flavors,flavors.length);
+		
+		//displaying all the functions
+		System.out.printf("%-20s %-20s\n\n","IceCream Name","Sold");
+		for(Flavors f : flavors) {
+			System.out.printf("%-20s %-20s\n",f.name,f.sold);
+		}
+	}
+	
+	//bubble sort function
+	public static void bubbleSort(Flavors flavors[], int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n-i-1;j++) {
+				if(flavors[j].sold<=flavors[j+1].sold) {
+					Flavors temp = flavors[j];
+					flavors[j] = flavors[j+1];
+					flavors[j+1] = temp;
+				}
+			}
+		}
+	}
+}
