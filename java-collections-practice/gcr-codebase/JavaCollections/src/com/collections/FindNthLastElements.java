2671b735e156a57ef3c68bafedd623f433b4266a
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-21 12:36:46 +0530

    [MohdMeraaz] : added Find the Nth Element from the End Code

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/collections/FindNthLastElements.java
@@ -0,0 +1,21 @@
+package com.collections;
+
+import java.util.LinkedList;
+
+public class FindNthLastElements {
+	public static void main(String[] args) {
+    	LinkedList<String> list = new LinkedList<>();
+    	list.add("A");
+    	list.add("B");
+    	list.add("C");
+    	list.add("D");
+    	list.add("E");
+    	int n = 2;
+    	int i =1;
+    	while(i<n) {
+    		list.pollLast();
+    		i++;
+    	}
+    	System.out.println("N Last Elemet is : "+list.pollLast());
+    }
+}
