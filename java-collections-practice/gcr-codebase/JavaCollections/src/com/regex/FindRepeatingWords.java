bad4642c4ce70517aee6d0317955c963a5c315a3
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-01-29 00:53:39 +0530

    [MohdMeraaz] : added FindRepeatingWords regex code.

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/regex/FindRepeatingWords.java
@@ -0,0 +1,18 @@
+package com.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FindRepeatingWords {
+	public static void main(String[] args) {
+		String regex = "\\b(\\w+)\\s+\\1\\b";
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		
+		String text = "This is is a repeated repeated word test.";
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+}
