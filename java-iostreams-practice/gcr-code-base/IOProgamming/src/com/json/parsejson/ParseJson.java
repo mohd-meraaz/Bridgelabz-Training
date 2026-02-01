eb63d35bb8f4e8a178b950e6ce2ad6e41bba1dff
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-02-01 14:50:08 +0530

    [MohdMeraaz] : Added parse json code

---
+++ java-iostreams-practice/gcr-code-base/IOProgamming/src/com/json/parsejson/ParseJson.java
@@ -0,0 +1,23 @@
+package com.json.parsejson;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ParseJson {
+	public static void main(String[] args) throws Exception{
+		File file = new File("DataFiles/child.json");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		JsonNode node = (JsonNode) mapper.readTree(file);
+		
+		for(JsonNode n : node) {
+			int age = n.get("age").asInt();
+			if(age>=25) {
+				System.out.println(n);
+			}
+		}
+	}
+}
