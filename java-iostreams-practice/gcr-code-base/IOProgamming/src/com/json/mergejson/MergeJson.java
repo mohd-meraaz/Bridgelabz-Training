32e471b499ca431f59e5c34adfe6f974292fc530
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-02-01 14:49:39 +0530

    [MohdMeraaz] : Added merge json code

---
+++ java-iostreams-practice/gcr-code-base/IOProgamming/src/com/json/mergejson/MergeJson.java
@@ -0,0 +1,23 @@
+package com.json.mergejson;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+
+public class MergeJson {
+	public static void main(String[] args) throws Exception{
+		File file1 = new File("DataFiles/student.json");
+		File file2 = new File("DataFiles/stduent2.json");
+		
+		ObjectMapper map = new ObjectMapper();
+		
+		ArrayNode array = (ArrayNode) map.readTree(file1);
+		ArrayNode array2 = (ArrayNode) map.readTree(file2);
+		
+		array.addAll(array2);
+		
+		System.out.println(array.toPrettyString());
+	}
+}
