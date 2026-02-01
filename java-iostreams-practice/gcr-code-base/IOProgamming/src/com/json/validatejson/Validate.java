5d011baf1c854b3b8b8876ae68810e4f87fd66f6
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-02-01 14:50:31 +0530

    [MohdMeraaz] : Added validate json code

---
+++ java-iostreams-practice/gcr-code-base/IOProgamming/src/com/json/validatejson/Validate.java
@@ -0,0 +1,25 @@
+package com.json.validatejson;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Validate {
+	public static void main(String[] args) throws IOException {
+		File file = new File("DataFiles/check.json");
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.readTree(file);
+			System.out.println("Format is valid.");
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File Not found");
+		}
+		catch(JsonParseException e) {
+			System.out.println("Format not valid!");
+		}
+	}
+}
