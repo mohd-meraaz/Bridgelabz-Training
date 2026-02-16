204dbbb90f7b30a57980a4148a80b4f4863c355b
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-02-16 11:51:55 +0530

    [MohdMeraaz] : Update DatabaseConnection.java

--- dbms-jdbc-practice/gcr-codebase/MysqlPractice/src/com/jdbcpractice/DatabaseConnection.java
+++ dbms-jdbc-practice/gcr-codebase/MysqlPractice/src/com/jdbcpractice/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DatabaseConnection {
 //	Database Essentials
-	private static final String URL = "jdbc:mysql://localhost:3306/practice_capge";
+	private static final String URL = "jdbc:mysql://localhost:3306/student_db";
 	private static final String USER = "root";
 	private static final String PASSWORD = "Meraaz@8904";
 	
