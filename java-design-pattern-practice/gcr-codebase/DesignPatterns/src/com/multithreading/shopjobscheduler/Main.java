880a1596bf031758989061b69e4a2910ee4bbfc9
Mohd Meraaz <mohdmeraaz08@Mohds-MacBook-Air.local>
2026-02-16 16:56:23 +0530

    [MohdMeraaz] : Updated shop job scheduler code

--- java-design-pattern-practice/gcr-codebase/DesignPatterns/src/com/multithreading/shopjobscheduler/Main.java
+++ java-design-pattern-practice/gcr-codebase/DesignPatterns/src/com/multithreading/shopjobscheduler/Main.java
@@ -1,5 +1,42 @@
 package com.multithreading.shopjobscheduler;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		Thread thread1 = new Thread(new PrintJob(new Job("Job1",10,5)));
+		thread1.setPriority(Thread.NORM_PRIORITY);
+		Thread thread2 = new Thread(new PrintJob(new Job("Job2",21,1)));
+		thread2.setPriority(Thread.MIN_PRIORITY);
+		Thread thread3 = new Thread(new PrintJob(new Job("Job3",53,3)));
+		thread3.setPriority(Thread.MIN_PRIORITY);
+		Thread thread4 = new Thread(new PrintJob(new Job("Job4",43,9)));
+		thread4.setPriority(Thread.MAX_PRIORITY);
+		Thread thread5 = new Thread(new PrintJob(new Job("Job5",5,7)));
+		thread5.setPriority(Thread.MAX_PRIORITY);
+		
+		System.out.println("Started Printing Job");
+		
+		long startTime = System.nanoTime();
+		
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread4.start();
+		thread5.start();
+		
+		try {
+			thread1.join();
+			thread2.join();
+			thread3.join();
+			thread4.join();
+			thread5.join();
+		}
+		catch(InterruptedException e) {
+			System.out.println(e);
+		}
+		
+		long endTime = System.nanoTime();
+		
+		System.out.println("...\nAll jobs completed in "+(endTime-startTime));
+	}
 }
--- java-design-pattern-practice/gcr-codebase/DesignPatterns/src/com/multithreading/shopjobscheduler/PrintJob.java
+++ java-design-pattern-practice/gcr-codebase/DesignPatterns/src/com/multithreading/shopjobscheduler/PrintJob.java
@@ -1,5 +1,40 @@
 package com.multithreading.shopjobscheduler;
 
-public class PrintJob {
+public class PrintJob implements Runnable{
+	Job job;
+	
+	public PrintJob(Job job) {
+		this.job = job;
+	}
 
+	@Override
+	public void run() {
+		print(job);
+	}
+	
+	public synchronized void print(Job job) {
+		String priority = findPriority(job.getPriority());
+		try {
+			System.out.println("[ "+priority+" Priority ] Printing "+job.getName()+" - "+ "pages 1 to "+job.getPage());
+			Thread.sleep(100);
+		}
+		catch(InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+	
+	public String findPriority(int priority) {
+		if(priority>=1 && priority<=4) {
+			return "LOW";
+		}
+		else if(priority>4 && priority<=6) {
+			return "MEDIUM";
+		}
+		else if(priority>6 && priority<=10) {
+			return "HIGHT";
+		}
+		else {
+			return "Invalid Priority";
+		}
+	}
 }
