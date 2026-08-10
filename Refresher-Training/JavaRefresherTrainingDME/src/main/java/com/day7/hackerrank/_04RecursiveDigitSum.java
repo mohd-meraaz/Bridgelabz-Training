a42f3b71305f8f4d97c3d48f039d833ce1174b7a
mohd-meraaz <meraaz0809@gmail.com>
2026-08-10 15:37:34 +0530

    [MohdMeraaz] : Added hackerrank solution of digit sum

--- Refresher-Training/JavaRefresherTrainingDME/src/main/java/com/day7/hackerrank/_04RecursiveDigitSum.java
+++ Refresher-Training/JavaRefresherTrainingDME/src/main/java/com/day7/hackerrank/_04RecursiveDigitSum.java
@@ -3,30 +3,21 @@ package com.day7.hackerrank;
 public class _04RecursiveDigitSum {
     public static int superDigit(String n, int k) {
 
-        int value = Integer.parseInt(n);
+        long sum = 0;
 
-        int a = digitSum(value);
-
-        int answer = a * k;
-
-        if (answer < 10) {
-            return answer;
-        } else {
-            return superDigit(String.valueOf(answer), 1);
+        for(char c : n.toCharArray()){
+            sum += c - '0';
         }
-    }
+        sum *= k;
 
-    public static int digitSum(int n) {
-
-        if (n == 0) {
-            return 0;
+        if(sum < 10){
+            return (int) sum;
         }
 
-        int lastDigit = n % 10;
-        int remaining = n / 10;
-
-        return lastDigit + digitSum(remaining);
+        return superDigit(String.valueOf(sum), 1);
     }
+
+
 
     static void main() {
         String n = "13450";
