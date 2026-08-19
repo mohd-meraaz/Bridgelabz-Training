cc8c8fd71048d89cd43a45114a7d7e39f0fcbccd
mohd-meraaz <meraaz0809@gmail.com>
2026-08-19 20:07:00 +0530

    [MohdMeraaz] : Added oops program of java inheritance

---
+++ Refresher-Training/JavaRefresherTrainingDME/src/main/java/com/day14/hackerrank/_01JavaInheritanceI.java
@@ -0,0 +1,27 @@
+package com.day14.hackerrank;
+
+public class _01JavaInheritanceI {
+    class Animal{
+        void walk(){
+            System.out.println("I am walking");
+        }
+    }
+
+    class Bird extends Animal {
+        void fly() {
+            System.out.println("I am flying");
+        }
+        void sing() {
+            System.out.println("I am singing");
+        }
+    }
+
+    void main(){
+
+            Bird bird = new Bird();
+            bird.walk();
+            bird.fly();
+            bird.sing();
+        }
+
+}
