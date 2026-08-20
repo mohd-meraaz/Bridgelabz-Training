646cd1ab1524349f1995f602413e92177a591065
mohd-meraaz <meraaz0809@gmail.com>
2026-08-20 12:45:19 +0530

    [MohdMeraaz] : Added hackerank oops program of java comparator

---
+++ Refresher-Training/JavaRefresherTrainingDME/src/main/java/com/day15/hackerrank/_02JavaComparator.java
@@ -0,0 +1,58 @@
+package com.day15.hackerrank;
+import java.io.*;
+import java.util.*;
+
+
+public class _02JavaComparator {
+
+
+    class Player {
+        String name;
+        int score;
+
+        Player(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+    class Checker implements Comparator<Player> {
+
+        @Override
+        public int compare(Player a, Player b) {
+
+            // Higher score first
+            if (a.score != b.score) {
+                return Integer.compare(b.score, a.score);
+            }
+
+            // Same score -> alphabetical name
+            return a.name.compareTo(b.name);
+        }
+    }
+
+
+
+        public  void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            Player[] players = new Player[n];
+
+            for (int i = 0; i < n; i++) {
+                String name = sc.next();
+                int score = sc.nextInt();
+
+                players[i] = new Player(name, score);
+            }
+
+            Arrays.sort(players, new Checker());
+
+            for (Player player : players) {
+                System.out.println(player.name + " " + player.score);
+            }
+
+    }
+}
