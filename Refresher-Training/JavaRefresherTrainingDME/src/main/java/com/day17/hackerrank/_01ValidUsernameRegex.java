8569fb882aae2faa939f3bcbffa96d248ae7ba42
mohd-meraaz <meraaz0809@gmail.com>
2026-08-24 16:02:27 +0530

    [MohdMeraaz] : Added hackerank Question's solution of Valid Username Regular Expression

---
+++ Refresher-Training/JavaRefresherTrainingDME/src/main/java/com/day17/hackerrank/_01ValidUsernameRegex.java
@@ -0,0 +1,24 @@
+package com.day17.hackerrank;
+
+import java.util.Scanner;
+
+public class _01ValidUsernameRegex {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        Sc.nextLine();
+        for(int i = 0 ; i<n  ; i++){
+            String st = Sc.nextLine();
+
+            String regex = "[a-zA-Z][a-zA-Z0-9_]{7,29}";
+
+            if(st.matches(regex)){
+                System.out.println("Valid");
+            }
+            else{
+                System.out.println("Invalid");
+            }
+
+        }
+    }
+}
